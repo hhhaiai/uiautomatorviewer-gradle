d350721c05463b85f1a83d196fe321b1f7a0bb59
TarCV <tarcv@users.noreply.github.com>
2024-06-09 08:39:12 +0300

    Fix JAVA_HOME handling

--- nix-run.sh
+++ nix-run.sh
@@ -3,5 +3,5 @@ set -e
 
 pushd "$(dirname "$(readlink -f "$0")")"
 ./gradlew installDist
-JAVA_HOME=$JAVA21_HOME ./build/install/uiautomatorviewer-gradle/bin/uiautomatorviewer-gradle
+./build/install/uiautomatorviewer-gradle/bin/uiautomatorviewer-gradle
 popd
